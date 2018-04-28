26ea13261eca8cb502974ab3658accb8ac507b31
Mateus Queiroz Correia <mateus.queiroz@bh01.local>
2018-04-28 19:19:43 -0300

    utilizando momment.js e filtrando data

--- locations.js
+++ locations.js
@@ -1,4 +1,20 @@
 var locations = [
-    {position: {lat: -19.9351086, lng: -43.9382402}, name: "Meu Evento", people: 500, finishDateTime: "28/4(Hoje) 22h"},
-    {position: {lat: -19.9371086, lng: -43.9382602}, name: "Meu Evento2", people: 1500, finishDateTime: "28/4(Hoje) 22h"},
-  ]
+  {
+    position: {lat: -19.9451086, lng: -43.9482402}, 
+    name: "Meu Evento de ontem", 
+    people: 500,
+    finishDateTime: moment("2018-04-27 20:00:00")
+  },
+  {
+    position: {lat: -19.9351086, lng: -43.9382402}, 
+    name: "Meu Evento", 
+    people: 500,
+    finishDateTime: moment("2018-04-28 20:00:00")
+  },
+{
+      position: {lat: -19.9371086, lng: -43.9382602}, 
+      name: "Meu Evento2", 
+      people: 1500, 
+      finishDateTime: moment("2018-04-28 21:00:00")
+    },
+  ];
