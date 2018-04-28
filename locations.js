95a01fb603eceb932bc8824d5d9f097b74bda56e
Mateus Queiroz Correia <mateus.queiroz@bh01.local>
2018-04-28 18:28:56 -0300

    versao inicial

---
+++ locations.js
@@ -0,0 +1,4 @@
+var locations = [
+    {position: {lat: -19.9351086, lng: -43.9382402}, name: "Meu Evento", people: 500, finishDateTime: "28/4(Hoje) 22h"},
+    {position: {lat: -19.9371086, lng: -43.9382602}, name: "Meu Evento2", people: 1500, finishDateTime: "28/4(Hoje) 22h"},
+  ]
